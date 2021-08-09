416c03ed8f6d8997ff4be0c34cb1f18466871c43
Paul Kuruvilla <rohitpaulk@gmail.com>
2021-08-09 23:26:48 +0100

    refactored

--- app/main.py
+++ app/main.py
@@ -18,60 +18,37 @@ SQLITE_SCHEMA_TABLE = Table(
 
 
 def read_sqlite_schema_records(database_file_path):
-    with open(database_file_path, "rb") as database_file:
-        database_file.seek(100)  # Skip the header section
-
-        _page_type = int.from_bytes(database_file.read(1), "big")
-        _first_freeblock_start = int.from_bytes(database_file.read(2), "big")
-        number_of_cells = int.from_bytes(database_file.read(2), "big")
-
-        database_file.seek(100 + 8)  # Skip the database header & b-tree page header, get to the cell pointer array
-
-        cell_pointers = [int.from_bytes(database_file.read(2), "big") for _ in range(number_of_cells)]
-
-        sqlite_schema_records = []
-
-        # Each of these cells represents a row in the sqlite_schema table.
-        for cell_pointer in cell_pointers:
-            database_file.seek(cell_pointer)
-            _number_of_bytes_in_payload = parse_varint(database_file)
-            rowid = parse_varint(database_file)
-            sqlite_schema_records.append(parse_record(database_file, SQLITE_SCHEMA_TABLE))
-
-        return [sqlite_schema_record for sqlite_schema_record in sqlite_schema_records if sqlite_schema_record["tbl_name"] != b"sqlite_sequence"]
+    rows = read_table_rows(database_file_path, SQLITE_SCHEMA_TABLE)
+    return [row for row in rows if row["tbl_name"] != b"sqlite_sequence"]
 
 
-def read_table_rows(database_file_path: str, rootpage: int):
+def read_table_rows(database_file_path: str, table: Table):
+    page_header_size = 100 if table.root_page == 1 else 0
+
     with open(database_file_path, "rb") as database_file:
         database_file.seek(16)  # Header string
         page_size = int.from_bytes(database_file.read(2), "big")
 
-        page_start = (rootpage - 1) * page_size
-        database_file.seek(page_start)
+        page_start = (table.root_page - 1) * page_size
+        database_file.seek(page_start + page_header_size)
 
         _page_type = int.from_bytes(database_file.read(1), "big")
         _first_freeblock_start = int.from_bytes(database_file.read(2), "big")
         number_of_cells = int.from_bytes(database_file.read(2), "big")
 
-        database_file.seek(page_start + 8)
+        database_file.seek(page_start + page_header_size + 8)
         cell_pointers = [int.from_bytes(database_file.read(2), "big") for _ in range(number_of_cells)]
 
-        # for cell_pointer in cell_pointers:
-        #     database_file.seek(page_start + cell_pointer)
-        #     _number_of_bytes_in_payload = parse_varint(database_file)
-        #     rowid = parse_varint(database_file)
-        #     record = parse_record(database_file, 5)
-        #
-        #     # Table contains columns: type, name, tbl_name, rootpage, sql
-        #     sqlite_schema_rows.append({
-        #         'type': record[0],
-        #         'name': record[1],
-        #         'tbl_name': record[2],
-        #         'rootpage': record[3],
-        #         'sql': record[4],
-        #     })
+        records = []
 
-        return cell_pointers
+        # Each of these cells represents a row in the sqlite_schema table.
+        for cell_pointer in cell_pointers:
+            database_file.seek(page_start + cell_pointer)
+            _number_of_bytes_in_payload = parse_varint(database_file)
+            rowid = parse_varint(database_file)
+            records.append(parse_record(database_file, table))
+
+        return records
 
 
 def handle_dot_command(command):
@@ -103,7 +80,7 @@ def execute_statement(statement):
     if statement.startswith("select count(*)"):
         table_name = statement.split(" ")[-1]
         table = get_table(database_file_path, table_name)
-        rows = read_table_rows(database_file_path, table.root_page)
+        rows = read_table_rows(database_file_path, table)
         print(len(rows))
     elif statement.startswith("select"):
         column_name = statement.split(" ")[1]
